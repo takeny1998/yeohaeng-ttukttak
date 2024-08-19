72e22a0435dbc7a4a40c0d9529772bcbb4a3260f
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-08-19 21:46:29 +0900

    feat: JPA `BaseTimeEntity` 생성 및 적용

--- server/src/main/java/com/yeohaeng_ttukttak/server/ServerApplication.java
+++ server/src/main/java/com/yeohaeng_ttukttak/server/ServerApplication.java
@@ -2,8 +2,10 @@ package com.yeohaeng_ttukttak.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaAuditing
 @EnableJpaRepositories
 @SpringBootApplication
 public class ServerApplication {
