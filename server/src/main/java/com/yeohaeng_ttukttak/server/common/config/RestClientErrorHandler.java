fd660122b10b46757aff2c6df8a1c66742a946ca
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-08-22 13:20:33 +0900

    refactor: RestClient 인터셉터, 핸들러 분리, 빈 구조 개편
    - 코드가 너무 길어서 인터셉터, 핸들러 코드를 별도 클래스로 분리함
    - Apple 연동을 위해 RestClientBuilder를 빈으로 제공하는 방식으로 개편
    - 애플리케이션 시작 Spring Boot 실행 시 애플리케이션 루트 밑에만 로깅 레벨을 debug로 변경
    (나중에 Production 배포 시 변경 필요)

---
+++ server/src/main/java/com/yeohaeng_ttukttak/server/common/config/RestClientErrorHandler.java
@@ -0,0 +1,45 @@
+package com.yeohaeng_ttukttak.server.common.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static org.springframework.web.client.RestClient.ResponseSpec.ErrorHandler;
+
+@Slf4j
+public class RestClientErrorHandler implements  ErrorHandler {
+
+    @Override
+    public void handle(HttpRequest request, ClientHttpResponse response) throws IOException {
+
+        String shortUUID = request.getHeaders()
+                .getFirst("Request-Trace-UUID");
+
+        try (BufferedReader bodyReader = new BufferedReader(
+                new InputStreamReader(response.getBody()))) {
+
+
+            log.error("[{}] >> {} {}", shortUUID, request.getMethod(), request.getURI());
+
+            request.getHeaders()
+                    .forEach((key, values) -> {
+                        log.error("[{}] >> -- {}: {}", shortUUID, key, values);
+                    });
+
+            log.error("[{}] << {}", shortUUID, response.getStatusCode());
+
+            String line;
+            while ((line = bodyReader.readLine()) != null) {
+                log.error("[{}] <<-- {}", shortUUID, line);
+            }
+
+        } catch(IOException e) {
+            log.error("[{}] [!] {}", shortUUID, e.getMessage());
+        }
+    }
+
+}
