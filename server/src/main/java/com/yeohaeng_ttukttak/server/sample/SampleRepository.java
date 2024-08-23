5f6d5ac342d4afbc375532cefd7d3f5dd2ddad04
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-08-23 15:25:37 +0900

    chore: 사용하지 않는 코드 삭제

--- server/src/main/java/com/yeohaeng_ttukttak/server/sample/SampleRepository.java
+++
@@ -1,6 +0,0 @@
-package com.yeohaeng_ttukttak.server.sample;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface SampleRepository extends JpaRepository<SampleEntity, Long> { }
