8711583ad52c0a722244f3632aa266544b9fe298
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-08-26 22:27:52 +0900

    chore: AopConfig 작성

--- server/src/main/java/com/yeohaeng_ttukttak/server/common/aop/aspect/AuthorizationAspect.java
+++ server/src/main/java/com/yeohaeng_ttukttak/server/common/aop/aspect/AuthorizationAspect.java
@@ -12,13 +12,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Slf4j
 @Aspect
-@Component
 @RequiredArgsConstructor
 public class AuthorizationAspect {
 
