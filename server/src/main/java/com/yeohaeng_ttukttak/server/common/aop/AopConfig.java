294a63a730637baffefd1da0048625c1176e3934
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-09-24 15:34:06 +0900

    fix-api: AuthorizationAspect 작동하지 않는 문제
    - Pointcut 지정 방식을 메서드 어노테이션으로 변경, 빈 등록 누락 수정

---
+++ server/src/main/java/com/yeohaeng_ttukttak/server/common/aop/AopConfig.java
@@ -0,0 +1,18 @@
+package com.yeohaeng_ttukttak.server.common.aop;
+
+import com.yeohaeng_ttukttak.server.domain.auth.service.AccessTokenService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AopConfig {
+
+    @Bean
+    public AuthorizationAspect authorizationAspect(
+            AccessTokenService accessTokenService,
+            HttpServletRequest httpServletRequest
+    ) {
+        return new AuthorizationAspect(accessTokenService, httpServletRequest);
+    }
+}
