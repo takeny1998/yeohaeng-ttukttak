787f5f1d4d0006867fd38ce62ec2da238943a2e6
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-09-25 17:08:01 +0900

    chore-api: 기존 애플리케이션 코드 제거

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,106 +1,50 @@
 import 'dart:async';
+import 'dart:ui';
 
-import 'package:application/features/notification/data/model/notification_model.dart';
-import 'package:application/features/notification/presentation/provider/notification_state_notifier.dart';
-import 'package:application/firebase_options.dart';
-import 'package:device_info_plus/device_info_plus.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:application_new/common/loading/async_loading_provider.dart';
+import 'package:application_new/common/log/logger.dart';
+import 'package:application_new/common/session/session_provider.dart';
+import 'package:application_new/feature/authentication/service/auth_service_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hive_flutter/adapters.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:logger/logger.dart';
 
-import 'config/router/router_provider.dart';
+import 'common/exception/exception.dart';
+import 'common/router/router_provider.dart';
 
-part 'main.g.dart';
+final messengerKey = GlobalKey<ScaffoldMessengerState>();
 
-Future<void> notificationConsumeHandler(
-    RemoteMessage message, WidgetRef ref) async {
-  print(
-      '[notificationConsumeHandler] id = ${message.messageId} data = ${message.data}, body = ${message.notification?.body}');
+void main() {
+  final logger = Logger();
+  final providerContainer = ProviderContainer();
 
-  final model = NotificationModel.fromRemoteMessage(message);
-  ref.read(notificationStateNotifierProvider.notifier).handle(model);
-}
+  FlutterError.onError = (error) {
+    logger.e('[HttpException] exception = $error');
+  };
 
-Future<void> notificationRegisterHandler(
-    RemoteMessage message, WidgetRef ref) async {
-  print(
-      '[notificationRegisterHandler] id = ${message.messageId} data = ${message.data}, body = ${message.notification?.body}');
+  PlatformDispatcher.instance.onError = (error, stack) {
+    if (error is BusinessException) {
+      logger.e('[HttpException] exception = $error');
+      switch (error) {
+        case ServerException(:final message):
+          messengerKey.currentState
+              ?.showSnackBar(SnackBar(content: Text(message)));
+        case NetworkException(:final code, :final message):
+          logger.e('[HttpException] code = $code, message = $message');
+        case AuthenticatedException():
+          final sessionNotifier =
+              providerContainer.read(sessionProvider.notifier);
+          sessionNotifier.update(isAuthenticated: false);
+      }
+    }
 
-  final model = NotificationModel.fromRemoteMessage(message);
-  ref.read(notificationStateNotifierProvider.notifier).register(model);
-}
+    return true;
+  };
 
-@pragma('vm:entry-point')
-Future<void> notificationBackgroundHandler(
-    RemoteMessage message, ProviderContainer container) async {
-  print(
-      '[notificationBackgroundHandler] id = ${message.messageId} data = ${message.data}, body = ${message.notification?.body}');
-
-  final model = NotificationModel.fromRemoteMessage(message);
-  container.read(notificationStateNotifierProvider.notifier).register(model);
-}
-
-@riverpod
-BaseDeviceInfo baseDeviceInfo(BaseDeviceInfoRef ref) {
-  throw UnimplementedError();
-}
-
-@riverpod
-String notificationToken(NotificationTokenRef ref) {
-  throw UnimplementedError();
-}
-
-Future<String> initNotifications() async {
-  //request permission from user (will prompt user)
-  await FirebaseMessaging.instance.requestPermission(
-      alert: true,
-      badge: true,
-      criticalAlert: true,
-      provisional: false,
-      sound: true);
-
-  // iOS foreground notification 권한
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-      alert: true, badge: true, sound: true);
-
-  //fetch the FCM token for this device
-  final fcmToken = await FirebaseMessaging.instance.getToken();
-
-  return fcmToken.toString();
-}
-
-Future<void> initHive() async {
-  await Hive.initFlutter();
-  await Hive.openBox("notifications");
-}
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  final firebaseApp = await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform);
-
-
-  print('[main()] firebaseApp: options = ${firebaseApp.options}');
-
-  final fcmToken = await initNotifications();
-  print('[main()] fcmToken: $fcmToken');
-
-  final deviceInfo = await DeviceInfoPlugin().deviceInfo;
-  await initHive();
-
-  final container = ProviderContainer(overrides: [
-    baseDeviceInfoProvider.overrideWithValue(deviceInfo),
-    notificationTokenProvider.overrideWithValue(fcmToken),
-  ]);
-
-  FirebaseMessaging.onBackgroundMessage(
-      (message) => notificationBackgroundHandler(message, container));
-
-  runApp(ProviderScope(parent: container, child: const MyApp()));
+  runApp(ProviderScope(
+    parent: providerContainer,
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends ConsumerStatefulWidget {
@@ -111,52 +55,48 @@ class MyApp extends ConsumerStatefulWidget {
 }
 
 class _MyAppState extends ConsumerState<MyApp> {
-  StreamSubscription<RemoteMessage>? onMessage, onMessageOpenedApp;
+  @override
+  void initState() {
+    Future.microtask(autoLogin);
+    super.initState();
+  }
+
+  FutureOr<void> autoLogin() async {
+    final authService = ref.read(authServiceProvider);
+    final sessionNotifier = ref.read(sessionProvider.notifier);
+
+    await authService.find();
+    sessionNotifier.update(isAuthenticated: true);
+  }
 
   @override
   Widget build(BuildContext context) {
-    final router = ref.watch(goRouterProvider);
+    final router = ref.watch(routerProvider);
+    final isLoading = ref.watch(asyncLoadingProvider).count > 0;
 
     return MaterialApp.router(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        routerConfig: router);
-  }
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routerConfig: router,
+      scaffoldMessengerKey: messengerKey,
+      builder: (context, widget) {
+        logger.d(isLoading);
 
-  @override
-  void initState() {
-    super.initState();
-    // 푸시 알림이 그냥 도착했을 때 동작한다.
-    onMessage = FirebaseMessaging.onMessage
-        .listen((message) => notificationRegisterHandler(message, ref));
-
-    // 푸시 알림을 받고, 사용자가 직접 클릭했을 때 동작한다.
-    onMessageOpenedApp = FirebaseMessaging.onMessageOpenedApp
-        .listen((message) => notificationConsumeHandler(message, ref));
-
-    Future.delayed(const Duration(seconds: 3), () async {
-      // 앱이 종료(terminated)된 상태에서,
-      // 사용자가 Push Notification을 클릭해 들어올 때 발생한다,
-      final initialMessage = await FirebaseMessaging.instance.getInitialMessage();
-      if (initialMessage == null) return;
-
-      final notification = NotificationModel.fromRemoteMessage(initialMessage);
-      print('[_MyAppState.initState] initialMessage = $notification ');
-
-      await ref
-          .read(notificationStateNotifierProvider.notifier)
-          .handle(notification);
-    });
-  }
-
-  @override
-  void dispose() {
-    onMessage?.cancel();
-    onMessageOpenedApp?.cancel();
-
-    super.dispose();
+        return Stack(
+          children: [
+            widget!,
+            if (isLoading)
+              Positioned.fill(
+                  child: Container(
+                color: Colors.white.withOpacity(0.5),
+                child: const Center(child: CircularProgressIndicator()),
+              ))
+          ],
+        );
+      },
+    );
   }
 }
