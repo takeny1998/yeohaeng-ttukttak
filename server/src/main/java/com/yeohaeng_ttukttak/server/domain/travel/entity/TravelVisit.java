98353b173e136aec476aeee2d5f8b08de8f0637b
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-10-02 20:00:28 +0900

    feat-api: 여행 방문 목적, 만족도 필드 추가

--- server/src/main/java/com/yeohaeng_ttukttak/server/domain/travel/entity/TravelVisit.java
+++ server/src/main/java/com/yeohaeng_ttukttak/server/domain/travel/entity/TravelVisit.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.ToString;
 
+import java.time.LocalDate;
+
 @Entity
 @ToString
 public class TravelVisit {
@@ -12,15 +14,28 @@ public class TravelVisit {
     @Id
     private Long id;
 
+    @NotNull
+    private LocalDate visitedOn;
+
+    @NotNull
+    private Integer seq;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "place_id")
     private Place place;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "travel_id")
     private Travel travel;
 
-    @NotNull
-    private int order;
+    @Enumerated(EnumType.STRING)
+    private VisitReason reason;
+
+    private Integer satisfaction;
+
+    private Integer revisit;
+
+    private Integer recommend;
 
 }
