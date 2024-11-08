96d850e8c229de6e9d30675630da18901fa90e4d
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-11-08 15:15:15 +0900

    feat-app(관광지-추천): 라벨 Line Break 적용

--- application/lib/feature/travel_plan/travel_plan_recommend/page/city_recommend_place_view.dart
+++ application/lib/feature/travel_plan/travel_plan_recommend/page/city_recommend_place_view.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:application_new/common/util/string_extension.dart';
 import 'package:application_new/common/util/translation.dart';
 import 'package:application_new/feature/travel_plan/city_place_pois/component/place_metric_list_item.dart';
@@ -7,14 +9,12 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class CityRecommendPlaceView extends StatelessWidget {
-
   final PlaceRecommendModel placeRecommend;
 
   const CityRecommendPlaceView({super.key, required this.placeRecommend});
 
   @override
   Widget build(BuildContext context) {
-
     final PlaceRecommendModel(:places, :categoryType) = placeRecommend;
 
     // 외부 Indicator 크기 변동을 위해 높이를 부여
@@ -25,28 +25,24 @@ class CityRecommendPlaceView extends StatelessWidget {
     final trKey = baseKey('travel_plan_recommend');
 
     const labelStyle = TextStyle(fontSize: 21.0, fontWeight: FontWeight.w600);
-    final labelText = trKey('label_place.${categoryType.name}')
-        .tr().lineBreakByWord();
+    final labelText = trKey('label_place.${categoryType.name}').tr();
 
-    return  Column(
+    return Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            width: 272.0,
+              width: 272.0,
               padding: const EdgeInsets.only(left: 24.0),
-              child: Text(
-                  labelText,
-                  style: labelStyle,
-                  softWrap: true)),
-          const SizedBox(height: 16.0),
+              child: Text(labelText, style: labelStyle, softWrap: true)),
+          const SizedBox(height: 8.0),
           SingleChildScrollView(
               child: Column(children: [
-                for (final place in places) ...[
-                  PlaceMetricListItem(placeMetric: PlaceMetricModel(place: place)),
-                  const SizedBox(width: 24.0),
-                ],
-              ])),
+            for (final place in places) ...[
+              PlaceMetricListItem(placeMetric: PlaceMetricModel(place: place)),
+              const SizedBox(width: 24.0),
+            ],
+          ])),
           const SizedBox(height: 48.0),
         ]);
   }
