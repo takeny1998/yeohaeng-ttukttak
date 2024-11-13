edde73b788cd93137179b0ffb26ad52be2cbe912
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-11-13 17:00:29 +0900

    refactor-api(여행-상세-조회): 관광지 및 방문 도메인으로 응답 분리
    - 관광지, 방문 목록을 한번에 반환하는 `GET /travels/{id}/detail` 관광지 및 방문 도메인으로 분리하였음

---
+++ server/src/main/java/com/yeohaeng_ttukttak/server/application/place/controller/PlaceController.java
@@ -0,0 +1,33 @@
+package com.yeohaeng_ttukttak.server.application.place.controller;
+
+import com.yeohaeng_ttukttak.server.application.place.controller.dto.FindPlaceResponse;
+import com.yeohaeng_ttukttak.server.common.dto.ServerResponse;
+import com.yeohaeng_ttukttak.server.common.exception.exception.fail.EntityNotFoundException;
+import com.yeohaeng_ttukttak.server.domain.place.dto.PlaceDto;
+import com.yeohaeng_ttukttak.server.domain.place.entity.Place;
+import com.yeohaeng_ttukttak.server.domain.place.repository.PlaceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/places")
+@RequiredArgsConstructor
+public class PlaceController {
+
+    private final PlaceRepository placeRepository;
+
+    @GetMapping("/{id}")
+    @Transactional(readOnly = true)
+    public ServerResponse<FindPlaceResponse> find(@PathVariable Long id) {
+
+        final Place place = placeRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+        
+        return new ServerResponse<>(new FindPlaceResponse(PlaceDto.of(place)));
+    }
+
+}
--- server/src/main/java/com/yeohaeng_ttukttak/server/application/travel/controller/TravelController.java
+++ server/src/main/java/com/yeohaeng_ttukttak/server/application/travel/controller/TravelController.java
@@ -4,13 +4,14 @@ import com.yeohaeng_ttukttak.server.application.travel.controller.dto.*;
 import com.yeohaeng_ttukttak.server.application.travel.service.CreateTravelService;
 import com.yeohaeng_ttukttak.server.application.travel.service.FindMyAllTravelService;
 import com.yeohaeng_ttukttak.server.application.travel.service.FindTravelsByCityService;
-import com.yeohaeng_ttukttak.server.application.travel.service.FindTravelDetailService;
+import com.yeohaeng_ttukttak.server.application.travel.service.FindTravelVisitsService;
 import com.yeohaeng_ttukttak.server.application.travel.service.dto.FindTravelsByCityResponse;
 import com.yeohaeng_ttukttak.server.common.aop.annotation.Authorization;
 import com.yeohaeng_ttukttak.server.common.dto.ServerResponse;
 import com.yeohaeng_ttukttak.server.common.exception.exception.fail.EntityNotFoundException;
 import com.yeohaeng_ttukttak.server.domain.auth.dto.AccessTokenDto;
 import com.yeohaeng_ttukttak.server.domain.travel.dto.TravelDto;
+import com.yeohaeng_ttukttak.server.domain.travel.dto.TravelVisitDto;
 import com.yeohaeng_ttukttak.server.domain.travel.entity.Travel;
 import com.yeohaeng_ttukttak.server.domain.travel.repository.TravelRepository;
 import jakarta.validation.Valid;
@@ -29,7 +30,7 @@ public class TravelController {
 
     private final CreateTravelService createTravelService;
     private final FindTravelsByCityService findTravelsByCityService;
-    private final FindTravelDetailService findTravelDetailService;
+    private final FindTravelVisitsService findTravelVisitsService;
     private final FindMyAllTravelService findMyAllTravelService;
     private final TravelRepository travelRepository;
 
@@ -66,11 +67,12 @@ public class TravelController {
                 new FindTravelsByCityResponse(dtos));
     }
 
-    @GetMapping("/{id}/detail")
-    public ServerResponse<FindTravelDetailResponse> findDetail(
+    @GetMapping("/{id}/visits")
+    public ServerResponse<FindTravelVisitsResponse> findVisits(
             @PathVariable Long id) {
-        return new ServerResponse<>(
-                findTravelDetailService.call(id).toResponse());
+        List<TravelVisitDto> dtoList = findTravelVisitsService.call(id);
+
+        return new ServerResponse<>(new FindTravelVisitsResponse(dtoList));
     }
 
     @GetMapping("/members/me")
