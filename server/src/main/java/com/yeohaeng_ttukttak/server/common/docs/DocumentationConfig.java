4e6a07e3be64e0304036bf563173cbf2455090ee
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-01-06 16:57:04 +0900

    docs-api(인증): 사용자 정보를 API 문서에서 제거

--- server/src/main/java/com/yeohaeng_ttukttak/server/common/docs/DocumentationConfig.java
+++ server/src/main/java/com/yeohaeng_ttukttak/server/common/docs/DocumentationConfig.java
@@ -1,11 +1,13 @@
 package com.yeohaeng_ttukttak.server.common.docs;
 
 import com.yeohaeng_ttukttak.server.common.aop.annotation.Authorization;
+import com.yeohaeng_ttukttak.server.domain.auth.dto.AuthenticationContext;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.OperationCustomizer;
@@ -13,10 +15,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.HandlerMethod;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 @OpenAPIDefinition(info = @Info(
         title = "여행 뚝딱 API Docs",
@@ -49,23 +49,45 @@ public class DocumentationConfig {
     }
 
     @Bean
-    public OperationCustomizer authorizationChecker() {
+    public OperationCustomizer customOperationCustomizer() {
         return new OperationCustomizer() {
             @Override
             public Operation customize(Operation operation, HandlerMethod handlerMethod) {
 
-                final Authorization authorizationAnnotation
-                        = handlerMethod.getMethodAnnotation(Authorization.class);
-
-                if (Objects.isNull(authorizationAnnotation)) {
-                    operation.setSecurity(Collections.emptyList());
-                } else {
-                    operation.setSecurity(List.of(securityRequirement()));
-                }
+                handleAuthorization(operation, handlerMethod);
 
                 return operation;
             }
         };
+    }
+
+
+    /**
+     * 사용자 인증 관련 API 문서 정보를 처리합니다.
+     * <ul>
+     *     <li>사용자 정보(AuthenticationContext) 파라미터를 API 문서에서 제거합니다.</li>
+     *     <li>인증이 필요한 메서드를 API 문서에 표시합니다.</li>
+     * </ul>
+     */
+    private void handleAuthorization(Operation operation, HandlerMethod handlerMethod) {
+
+        if (Objects.nonNull(operation.getParameters())) {
+
+            final List<Parameter> parameters = operation.getParameters().stream()
+                    .filter(parameter -> parameter.getClass().isInstance(AuthenticationContext.class))
+                    .toList();
+
+            operation.setParameters(parameters);
+        }
+
+        final Authorization authorizationAnnotation
+                = handlerMethod.getMethodAnnotation(Authorization.class);
+
+        if (Objects.isNull(authorizationAnnotation)) {
+            return;
+        }
+
+        operation.setSecurity(List.of(securityRequirement()));
     }
 
     private SecurityRequirement securityRequirement() {
