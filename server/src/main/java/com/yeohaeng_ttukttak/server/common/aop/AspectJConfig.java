d8c531af7ca92b0ec2bd957d8ae185ee748f26cc
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-01-10 15:27:06 +0900

    feat-api: Native AspectJ로 마이그레이션
    - Spring의 LTW 대신 좀 더 빠르고 안전한 AspectJWeaver만 사용하도록 구성

--- server/src/main/java/com/yeohaeng_ttukttak/server/common/aop/AspectJConfig.java
+++
@@ -1,8 +0,0 @@
-package com.yeohaeng_ttukttak.server.common.aop;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableLoadTimeWeaving;
-
-@Configuration
-@EnableLoadTimeWeaving(aspectjWeaving = EnableLoadTimeWeaving.AspectJWeaving.ENABLED)
-public class AspectJConfig { }
